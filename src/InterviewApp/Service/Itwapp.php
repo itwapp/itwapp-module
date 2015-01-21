69e9a8e36f00bcee14310345c0d95c6c1e60cf52
Gaël CRISPYN <gcrispyn@assessfirst.com>
2015-01-21 17:44:34 +0100

    Adding Itwapp service

---
+++ src/InterviewApp/Service/Itwapp.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace InterviewApp\Service;
+
+/**
+ * Description of Itwapp
+ *
+ * @author gael
+ */
+class Itwapp extends \Itwapp
+{
+}
