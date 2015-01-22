5fce3be4e2e2ced0ecbd4e2c484a2da5169dc3c2
Gaël CRISPYN <gcrispyn@assessfirst.com>
2015-01-22 15:35:27 +0100

    Adding new methods in Itwapp service

--- src/InterviewApp/Service/Itwapp.php
+++ src/InterviewApp/Service/Itwapp.php
@@ -15,4 +15,38 @@ namespace InterviewApp\Service;
  */
 class Itwapp
 {
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param \GuzzleHttp\Client $client
+     * @return \InterviewApp\Service\Itwapp
+     */
+    public function setClient(\GuzzleHttp\Client $client)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function createInterview($name, array $questions, $video, $text, $callback = 'http://itwapp.io')
+    {
+    }
+
+    protected function buildSignature($url, $mode, $secretKey)
+    {
+        $hmac = base64_encode(hash_hmac('sha256', $mode . ':' . $url, $secretKey, true));
+
+        return md5($hmac);
+    }
 }
