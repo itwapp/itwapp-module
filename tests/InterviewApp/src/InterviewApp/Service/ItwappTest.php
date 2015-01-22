adf14b4c9ddbc7a34c665511f2536e6d3072a802
Gaël CRISPYN <gcrispyn@assessfirst.com>
2015-01-22 15:41:13 +0100

    Update unit tests

--- tests/InterviewApp/src/InterviewApp/Service/ItwappTest.php
+++ tests/InterviewApp/src/InterviewApp/Service/ItwappTest.php
@@ -5,61 +5,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+namespace InterviewApp\Service;
 /**
  * Description of Itwapp
  *
  * @author gael
  */
-class Itwapp extends PHPUnit_Framework_TestCase
+class ItwappTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \InterviewApp\Service\Itwapp
+     */
     protected $instance;
+
+    /**
+     * @var GuzzleHttp\Client
+     */
+    protected $client;
 
     public function setUp()
     {
         $this->instance = new \InterviewApp\Service\Itwapp();
+        $this->client   = $this->getMock('\GuzzleHttp\Client', [], [], '', false);
+
+        $this->instance->setClient($this->client);
     }
 
-    public function testCreateInterview()
+    public function testSetClient()
     {
-        $interview = $this->instance->createInterview([
-            "name"      => "Assessfirst Interview",
-            "questions" => [
-                [
-                    "content"     => "question 1",
-                    "readingTime" => 60,
-                    "answerTime"  => 60,
-                    "number"      => 1
-                ]
-            ],
-            "video"     => "",
-            "text"      => "",
-            "callback"  => "http://myurl.com/done"
-        ]);
-        $this->assertInstanceOf('\Interview', $interview);
+        $this->client = $this->getMock('\GuzzleHttp\Client', [], [], '', false);
+
+        $this->assertSame($this->instance, $this->instance->setClient($this->client));
     }
 
-    public function testGetApplicant()
+    public function testGetClient()
     {
-        $this->assertSame($this->instance->createApplicant(), $this->instance->getApplicant());
+        $this->assertSame($this->client, $this->instance->getClient());
     }
 
-//    public function testCreateApplicant()
-//    {
-//        $applicant = $this->instance->createApplicant([
-//            'mail'      => 'test@test.fr',
-//            $interview, $dateBegin, $dateEnd, $dateAnswer, $emailView, $linkClicked, $firstname, $lastname, $lang, $videoLink, $text, $deleted, $callback = "http://itwapp.io"
-//        ]);
-//        $this->assertInstanceOf('\Applicant', $this->instance->createApplicant());
-//    }
-//
-//    public function testGetInterview()
-//    {
-//        $this->assertInstanceOf('\Interview', $this->instance->getApplicant());
-//    }
-}
+    public function testBuildSignature()
+    {
+        $url       = '/api/exemple?foo=bar&apiKey=YOUR_KEY_HERE&timestamp=CURRENT_TIMESTAMP_MILLIS';
+        $mode      = 'GET';
+        $secretKey = md5('test');
 
-class ApplicantStub extends Applicant
-{
+        $reflection = new \ReflectionClass(get_class($this->instance));
+        $method = $reflection->getMethod('buildSignature');
+        $method->setAccessible(true);
 
+        $this->assertEquals('55f8ec9b3e2bcbb2f4dc67ff7ded2bf6', $method->invokeArgs($this->instance, [$url, $mode, $secretKey]));
+    }
 }
