20d531889e23c24e0e19d8b2f6c6f1e41d2f5d4b
Gaël CRISPYN <gcrispyn@assessfirst.com>
2015-01-26 12:13:41 +0100

    Adding abstract dao class

---
+++ src/InterviewApp/DAO/DAOAbstract.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace InterviewApp\DAO;
+
+/**
+ * Description of DAOAbstract
+ *
+ * @author gael
+ */
+abstract class DAOAbstract
+{
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return get_object_vars($this);
+    }
+
+    /**
+     * @param array $data
+     * 
+     * @return \InterviewApp\DAO\DAOAbstract
+     */
+    public function setData(array $data)
+    {
+        foreach ($data as $attribute => $value) {
+            $this->$attribute = $value;
+        }
+
+        return $this;
+    }
+}
